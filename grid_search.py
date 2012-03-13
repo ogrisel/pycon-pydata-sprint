f4a26c72c917c73ede1ba8a784d7200a80254cf1
Olivier Grisel <olivier.grisel@ensta.org>
2012-03-13 16:41:06 -0700

    Starting API cleanup for the grid search example

--- grid_search.py
+++ grid_search.py
@@ -1,16 +1,15 @@
-"""
-The :mod:`sklearn.grid_search` includes utilities to fine-tune the parameters
-of an estimator.
+"""IPython.parallel implementation for Randomized Cross-Validated Grid Seach
+
+
 """
 
-# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>,
-#         Gael Varoquaux <gael.varoquaux@normalesup.org>
+
+# Author: MinRK <benjaminrk@gmail.com>
+#         Olivier Grisel <olivier.grisel@gmail.com>
 # License: BSD Style.
 
 import copy
-from itertools import product
 import random
-import time
 from collections import defaultdict
 from operator import itemgetter
 
@@ -19,74 +18,20 @@ import scipy.sparse as sp
 
 from IPython import parallel
 
-from sklearn.base import BaseEstimator, is_classifier, clone
+from sklearn.grid_search import IterGrid
+from sklearn.base import clone, is_classifier
 from sklearn.cross_validation import check_cv
-from sklearn.externals.joblib import Parallel, delayed, logger
-from sklearn.utils import deprecated
 
 
-class IterGrid(object):
-    """Generators on the combination of the various parameter lists given
-
-    Parameters
-    ----------
-    param_grid: dict of string to sequence
-        The parameter grid to explore, as a dictionary mapping estimator
-        parameters to sequences of allowed values.
-
-    Returns
-    -------
-    params: dict of string to any
-        **Yields** dictionaries mapping each estimator parameter to one of its
-        allowed values.
-
-    Examples
-    --------
-    >>> from sklearn.grid_search import IterGrid
-    >>> param_grid = {'a':[1, 2], 'b':[True, False]}
-    >>> list(IterGrid(param_grid)) #doctest: +NORMALIZE_WHITESPACE
-    [{'a': 1, 'b': True}, {'a': 1, 'b': False},
-     {'a': 2, 'b': True}, {'a': 2, 'b': False}]
-
-    See also
-    --------
-    :class:`GridSearchCV`:
-        uses ``IterGrid`` to perform a full parallelized grid search.
-    """
-
-    def __init__(self, param_grid):
-        self.param_grid = param_grid
-
-    def __iter__(self):
-        param_grid = self.param_grid
-        if hasattr(param_grid, 'items'):
-            # wrap dictionary in a singleton list
-            param_grid = [param_grid]
-        for p in param_grid:
-            # Always sort the keys of a dictionary, for reproducibility
-            items = sorted(p.items())
-            keys, values = zip(*items)
-            for v in product(*values):
-                params = dict(zip(keys, v))
-                yield params
-
-
-def fit_grid_point(X, y, base_clf, clf_params, train, test, loss_func,
-                   score_func, verbose, param_id=None, **fit_params):
+def fit_grid_point(X, y, base_clf, params, train, test, loss_func,
+                   score_func, param_id=None):
     """Run fit on one set of parameters
 
     Returns the score and the instance of the classifier
     """
-    if verbose > 1:
-        start_time = time.time()
-        msg = '%s' % (', '.join('%s=%s' % (k, v)
-                                     for k, v in clf_params.iteritems()))
-        print "[GridSearchCV] %s %s" % (msg, (64 - len(msg)) * '.')
-
     # update parameters of the classifier after a copy of its base structure
-    # FIXME we should be doing a clone here
     clf = copy.deepcopy(base_clf)
-    clf.set_params(**clf_params)
+    clf.set_params(**params)
 
     if isinstance(X, list) or isinstance(X, tuple):
         X_train = [X[i] for i, cond in enumerate(train) if cond]
@@ -121,7 +66,7 @@ def fit_grid_point(X, y, base_clf, clf_params, train, test, loss_func,
         y_test = None
         y_train = None
 
-    clf.fit(X_train, y_train, **fit_params)
+    clf.fit(X_train, y_train)
 
     if loss_func is not None:
         y_pred = clf.predict(X_test)
@@ -131,155 +76,14 @@ def fit_grid_point(X, y, base_clf, clf_params, train, test, loss_func,
         this_score = score_func(y_test, y_pred)
     else:
         this_score = clf.score(X_test, y_test)
-
-    if y is not None:
-        if hasattr(y, 'shape'):
-            this_n_test_samples = y.shape[0]
-        else:
-            this_n_test_samples = len(y)
-    else:
-        if hasattr(X, 'shape'):
-            this_n_test_samples = X.shape[0]
-        else:
-            this_n_test_samples = len(X)
-    if verbose > 2:
-        msg += ", score=%f" % this_score
-    if verbose > 1:
-        end_msg = "%s -%s" % (msg,
-                              logger.short_format_time(time.time() -
-                                                       start_time))
-        print "[GridSearchCV] %s %s" % ((64 - len(end_msg)) * '.', end_msg)
-    return param_id, clf_params, this_score
+    return param_id, params, this_score
 
 
-class IPythonGridSearchCV(BaseEstimator):
-    """Grid search on the parameters of a classifier
-
-    Important members are fit, predict.
-
-    GridSearchCV implements a "fit" method and a "predict" method like
-    any classifier except that the parameters of the classifier
-    used to predict is optimized by cross-validation.
-
-    Parameters
-    ----------
-    estimator: object type that implements the "fit" and "predict" methods
-        A object of that type is instantiated for each grid point.
-
-    param_grid: dict
-        Dictionary with parameters names (string) as keys and lists of
-        parameter settings to try as values.
-
-    loss_func: callable, optional
-        function that takes 2 arguments and compares them in
-        order to evaluate the performance of prediciton (small is good)
-        if None is passed, the score of the estimator is maximized
-
-    score_func: callable, optional
-        A function that takes 2 arguments and compares them in
-        order to evaluate the performance of prediction (high is good).
-        If None is passed, the score of the estimator is maximized.
-
-    fit_params : dict, optional
-        parameters to pass to the fit method
-
-    n_jobs: int, optional
-        number of jobs to run in parallel (default 1)
-
-    pre_dispatch: int, or string, optional
-        Controls the number of jobs that get dispatched during parallel
-        execution. Reducing this number can be useful to avoid an
-        explosion of memory consumption when more jobs get dispatched
-        than CPUs can process. This parameter can be:
-
-            - None, in which case all the jobs are immediatly
-              created and spawned. Use this for lightweight and
-              fast-running jobs, to avoid delays due to on-demand
-              spawning of the jobs
-
-            - An int, giving the exact number of total jobs that are
-              spawned
-
-            - A string, giving an expression as a function of n_jobs,
-              as in '2*n_jobs'
-
-    iid: boolean, optional
-        If True, the data is assumed to be identically distributed across
-        the folds, and the loss minimized is the total loss per sample,
-        and not the mean loss across the folds.
-
-    cv : integer or crossvalidation generator, optional
-        If an integer is passed, it is the number of fold (default 3).
-        Specific crossvalidation objects can be passed, see
-        sklearn.cross_validation module for the list of possible objects
-
-    refit: boolean
-        refit the best estimator with the entire dataset
-
-    verbose: integer
-        Controls the verbosity: the higher, the more messages.
-
-    Examples
-    --------
-    >>> from sklearn import svm, grid_search, datasets
-    >>> iris = datasets.load_iris()
-    >>> parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
-    >>> svr = svm.SVC()
-    >>> clf = grid_search.GridSearchCV(svr, parameters)
-    >>> clf.fit(iris.data, iris.target)
-    ...                             # doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
-    GridSearchCV(cv=None,
-        estimator=SVC(C=None, cache_size=..., coef0=..., degree=...,
-            gamma=..., kernel='rbf', probability=False,
-            scale_C=True, shrinking=True, tol=...),
-        fit_params={}, iid=True, loss_func=None, n_jobs=1,
-            param_grid=...,
-            ...)
-
-    Attributes
-    ----------
-    `grid_scores_` : dict of any to float
-        Contains scores for all parameter combinations in param_grid.
-
-    `best_estimator_` : estimator
-        Estimator that was choosen by grid search, i.e. estimator
-        which gave highest score (or smallest loss if specified)
-        on the left out data.
-
-    `best_score_` : float
-        score of best_estimator on the left out data.
-
-    Notes
-    ------
-    The parameters selected are those that maximize the score of the left out
-    data, unless an explicit score_func is passed in which case it is used
-    instead. If a loss function loss_func is passed, it overrides the score
-    functions and is minimized.
-
-    If `n_jobs` was set to a value higher than one, the data is copied for each
-    point in the grid (and not `n_jobs` times). This is done for efficiency
-    reasons if individual jobs take very little time, but may raise errors if
-    the dataset is large and not enough memory is available.  A workaround in
-    this case is to set `pre_dispatch`. Then, the memory is copied only
-    `pre_dispatch` many times. A reasonable value for `pre_dispatch` is 2 *
-    `n_jobs`.
-
-    See Also
-    ---------
-    :class:`IterGrid`:
-        generates all the combinations of a an hyperparameter grid.
-
-    :func:`sklearn.cross_validation.train_test_split`:
-        utility function to split the data into a development set usable
-        for fitting a GridSearchCV instance and an evaluation set for
-        its final evaluation.
-
-    """
+class IPythonGridSearchCV(object):
+    """Grid search on the parameters of an estimator w/ a scoring function"""
 
     def __init__(self, estimator, param_grid, loss_func=None, score_func=None,
-                 fit_params=None, n_jobs=1, iid=True, refit=True, cv=None,
-                 verbose=0, pre_dispatch='2*n_jobs', view=None,
-                ):
+                 cv=None, view=None):
         if not hasattr(estimator, 'fit') or \
            not (hasattr(estimator, 'predict') or hasattr(estimator, 'score')):
             raise TypeError("estimator should a be an estimator implementing"
@@ -297,19 +101,13 @@ class IPythonGridSearchCV(BaseEstimator):
         self.param_grid = param_grid
         self.loss_func = loss_func
         self.score_func = score_func
-        self.n_jobs = n_jobs
-        self.fit_params = fit_params if fit_params is not None else {}
-        self.iid = iid
-        self.refit = refit
         self.cv = cv
-        self.verbose = verbose
-        self.pre_dispatch = pre_dispatch
         self.view = view
         self._push_results = None
         self._fit_results = None
 
-    def fit(self, X, y=None, **params):
-        """Run fit with all sets of parameters
+    def fit_async(self, X, y=None):
+        """Run fit asynchronously with all sets of parameters
 
         Returns the best classifier
 
@@ -325,12 +123,9 @@ class IPythonGridSearchCV(BaseEstimator):
             None for unsupervised learning.
 
         """
-        
         import os
         import binascii
-        import itertools
         
-        self._set_params(**params)
         estimator = self.estimator
         cv = self.cv
         if hasattr(X, 'shape'):
@@ -350,7 +145,6 @@ class IPythonGridSearchCV(BaseEstimator):
         grid = IterGrid(self.param_grid)
         random.shuffle(list(grid))
         base_clf = clone(self.estimator)
-        pre_dispatch = self.pre_dispatch
         
         suffix = binascii.hexlify(os.urandom(10))
         
@@ -377,15 +171,15 @@ class IPythonGridSearchCV(BaseEstimator):
             for train,test in cv:
                 ars.append(self.view.apply_async(fit_grid_point,
                         rX, ry, base_clf, clf_params, train, test,
-                        self.loss_func, self.score_func, self.verbose,
-                        param_id=param_id, **self.fit_params)
+                        self.loss_func, self.score_func,
+                        param_id=param_id)
                 )
         
         # clear folllow dep
         self.view.follow = None
         
         self._fit_results = ars
-        return self
+        return len(ars)
 
     def collect_results(self):
         
@@ -426,31 +220,4 @@ class IPythonGridSearchCV(BaseEstimator):
         
         # sort by mean score
         results.sort(key=itemgetter(1), reverse=True)
-        if not_ready:
-            print "%i results remaining to compute" % len(not_ready)
-        return results
-        
-
-    def score(self, X, y=None):
-        if hasattr(self.best_estimator_, 'score'):
-            return self.best_estimator_.score(X, y)
-        if self.score_func is None:
-            raise ValueError("No score function explicitly defined, "
-                             "and the estimator doesn't provide one %s"
-                             % self.best_estimator_)
-        y_predicted = self.predict(X)
-        return self.score_func(y, y_predicted)
-
-    @property
-    @deprecated('GridSearchCV.best_estimator is deprecated'
-                ' and will be removed in version 0.12.'
-                ' Please use ``GridSearchCV.best_estimator_`` instead.')
-    def best_estimator(self):
-        return self.best_estimator_
-
-    @property
-    @deprecated('GridSearchCV.best_score is deprecated'
-                ' and will be removed in version 0.12.'
-                ' Please use ``GridSearchCV.best_score_`` instead.')
-    def best_score(self):
-        return self.best_score_
+        return results, len(not_ready)
